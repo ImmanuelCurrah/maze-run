09d4260ef27193a1108042168f0a14c41e96d531
Immanuel Currah <immanuelcurrah@Immanuels-MBP.lan>
2022-01-13 21:28:46 -0500

    finished my highscores functionality

--- maze-running/src/components/highscores/HighScores.jsx
+++ maze-running/src/components/highscores/HighScores.jsx
@@ -8,6 +8,8 @@ import classes from "./HighScores.module.css";
 export default function HighScores() {
   const [highScores, setHighScores] = useState({});
 
+  let bestScores = [];
+
   useEffect(() => {
     const fetchHighScoresData = async () => {
       let response = await api.get();
@@ -16,19 +18,30 @@ export default function HighScores() {
     fetchHighScoresData();
   }, []);
 
+  const sortScores = () => {
+    if (highScores.data) {
+      highScores.data.records.map((score) => {
+        bestScores.push(score);
+      });
+    }
+  };
+  sortScores();
+  let finalHighScores = bestScores.sort((a, b) => {
+    return a.fields.score - b.fields.score;
+  });
+
   return (
     <Fragment>
       <div className={classes.highscores}>
-        {highScores.data &&
-          highScores.data.records.map((score) => {
-            return (
-              <Card key={score.id}>
-                <h4>{`${score.fields.maze}`}</h4>
-                <h4>{`a MASSIVE score of: ${score.fields.score}`}</h4>
-                <Button destination={score.id}>See Details</Button>
-              </Card>
-            );
-          })}
+        {finalHighScores.map((score) => {
+          return (
+            <Card key={score.id}>
+              <h4>{`${score.fields.maze}`}</h4>
+              <h4>{`a MASSIVE score of: ${score.fields.score}`}</h4>
+              <Button destination={score.id}>See Details</Button>
+            </Card>
+          );
+        })}
       </div>
     </Fragment>
   );
